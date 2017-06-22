8be32c3a73bf39d0b27c93c7d0e1829636e5a8b0
Michela Paganini <michela.paganini@cern.ch>
2017-06-22 14:47:45 -0700

    Add BDT, FCN

--- train.py
+++ train.py
@@ -22,86 +22,177 @@ def get_parser():
     parser.add_argument('val_data', action='store', type=str,
                         help='path to HDF5 file to validate on')
 
+    parser.add_argument('model', action='store', type=str,
+                        help='one of: "3DCNN", "CNN", "FCN", "BDT"')
+
     return parser
 
 if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
 
+    # -- check that model makes sense:
+    if args.model not in ['3DCNN', 'CNN', 'FCN', 'BDT']:
+        raise ValueError("The model type needs to be one of 'CNN', 'FCN', 'BDT'.")
+
+    # -- load data:
     data = h5py.File(args.train_data)
     images = np.expand_dims(data['all_events']['hist'][:args.nb_events], -1)
     labels = data['all_events']['y'][:args.nb_events]
     weights = data['all_events']['weight'][:args.nb_events]
-#    weights = np.log(weights+1)                                                                                                                                                     
+#    weights = np.log(weights+1)
     weights = weights**0.1                                                                                                                                                       
-
     val = h5py.File(args.val_data)
     images_val = np.expand_dims(val['all_events']['hist'][:], -1)
     labels_val = val['all_events']['y'][:]
-    weights_val = val['all_events']['weight'][:]                                                                                                                                                       
+    weights_val = val['all_events']['weight'][:]  
 
-    from keras.layers import (Input, Conv2D, LeakyReLU,
-        BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
+    # -- output file names
+    model_weights = 'model_weights_weighted01_' + args.model + '.h5'
+    predictions_file = 'prediction_nn_weighted01_' + args.model + '.npy'
 
-    x = Input(shape=(64, 64, 1))
-    h = Conv2D(64, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(x)
-#    h = BatchNormalization()(h)                                                                                                                                                     
-#    h = MaxPooling2D(pool_size=(2, 2))(h)                                                                                                                                           
-#    h = Dropout(0.5)(h)                  
-    h = Conv2D(128, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
-#    h = BatchNormalization()(h)                                                                                                                                                     
-#    h = MaxPooling2D(pool_size=(2, 2))(h)                                                                                                                                           
-#    h = Dropout(0.5)(h)                  
-    h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(h)
-#    h = BatchNormalization()(h)                                                                                                                                                     
-#    h = MaxPooling2D(pool_size=(2, 2))(h)
-    h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
-#    h = BatchNormalization()(h)          
-    h = Flatten()(h)
-    h = Dense(512, activation='relu')(h)
-#    h = Dropout(0.5)(h)
-    y = Dense(1, activation='sigmoid')(h)
+    if args.model == 'BDT':
+        from sklearn.ensemble import GradientBoostingClassifier
+        from sklearn.model_selection import GridSearchCV
+        base_clf = GradientBoostingClassifier(verbose=2)
+        parameters = {
+            'n_estimators': [50, 100],
+            'max_depth':[3, 5]
+        }
+        clf = GridSearchCV(base_clf, parameters, n_jobs=4, fit_params={'sample_weight': weights})
+        clf.fit(images.reshape(images.shape[0], -1), labels)
+        yhat = clf.predict_proba(images_val.reshape(images_val.shape[0], -1))
+        np.save(predictions_file, yhat)
 
-    from keras.models import Model
-    model = Model(
-        inputs = x,
-        outputs = y
-    )
-    model.summary()
-    model.compile(
-        optimizer='adam',
-        loss='binary_crossentropy',
-        metrics=['accuracy']
-    )
+    elif args.model == 'CNN':
+        from keras.layers import (Input, Conv2D, LeakyReLU,
+            BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
+        from keras.models import Model
+        from keras.callbacks import EarlyStopping, ModelCheckpoint
 
-    from keras.callbacks import EarlyStopping, ModelCheckpoint
-    model_weights = 'model_weights_weighted05.h5'
-    try:
-        model.load_weights(model_weights)
-        print 'Weights loaded from ' + model_weights
-    except IOError:
-        print 'No pre-trained weights found'
-
-    try:
-        model.fit(images, labels,
-                  batch_size=args.batch_size,
-                  sample_weight=weights,
-                  epochs=args.nb_epochs,
-                  verbose=1,
-                  callbacks = [
-                    EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
-                    ModelCheckpoint(model_weights,
-                        monitor='val_loss', verbose=True, save_best_only=True)
-                  ],
-                  validation_data=(images_val, labels_val, weights_val)
+        x = Input(shape=(images.shape[1], images.shape[2], images.shape[3]))
+        h = Conv2D(64, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(x)
+        #    h = BatchNormalization()(h)                                                                                                                                                     
+        #    h = MaxPooling2D(pool_size=(2, 2))(h)                                                                                                                                           
+        #    h = Dropout(0.5)(h)                  
+        h = Conv2D(128, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
+        #    h = BatchNormalization()(h)                                                                                                                                                     
+        #    h = MaxPooling2D(pool_size=(2, 2))(h)                                                                                                                                           
+        #    h = Dropout(0.5)(h)                  
+        h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(h)
+        #    h = BatchNormalization()(h)                                                                                                                                                     
+        #    h = MaxPooling2D(pool_size=(2, 2))(h)
+        h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
+        #    h = BatchNormalization()(h)          
+        h = Flatten()(h)
+        h = Dense(512, activation='relu')(h)
+        #    h = Dropout(0.5)(h)
+        y = Dense(1, activation='sigmoid')(h)
+        model = Model(
+            inputs = x,
+            outputs = y
         )
-    except KeyboardInterrupt:
-        print 'Training finished early'
+        model.summary()
+        model.compile(
+            optimizer='adam',
+            loss='binary_crossentropy',
+            metrics=['accuracy']
+        )
+        try:
+            model.load_weights(model_weights)
+            print 'Weights loaded from ' + model_weights
+        except IOError:
+            print 'No pre-trained weights found'
+        try:
+            model.fit(images, labels,
+                      batch_size=args.batch_size,
+                      sample_weight=weights,
+                      epochs=args.nb_epochs,
+                      verbose=1,
+                      callbacks = [
+                        EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
+                        ModelCheckpoint(model_weights,
+                            monitor='val_loss', verbose=True, save_best_only=True)
+                      ],
+                      validation_data=(images_val, labels_val, weights_val)
+            )
+        except KeyboardInterrupt:
+            print 'Training finished early'
 
-    model.load_weights(model_weights)
-    yhat = model.predict(images_val, verbose=1, batch_size=args.batch_size)
-   # score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
-   # print 'Validation loss:', score[0]
-   # print 'Validation accuracy:', score[1]
-    np.save('prediction_nn_weighted05.npy', yhat)
+        model.load_weights(model_weights)
+        yhat = model.predict(images_val, verbose=1, batch_size=args.batch_size)
+       # score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
+       # print 'Validation loss:', score[0]
+       # print 'Validation accuracy:', score[1]
+        np.save(predictions_file, yhat)
+
+    elif args.model == 'FCN':
+        from keras.layers import (Input, Conv2D, LeakyReLU,
+            BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
+        from keras.models import Model
+        from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+        x = Input(shape=(images.shape[1] * images.shape[2], ))        
+        #h = Flatten()(x)
+        h = Dense(2048, activation='relu')(x)
+        h = Dropout(0.5)(h)
+        h = Dense(2048, activation='relu')(h)
+        h = Dropout(0.5)(h)
+        h = Dense(2048, activation='relu')(h)
+        h = Dropout(0.5)(h)
+        h = Dense(2048, activation='relu')(h)
+        h = Dropout(0.5)(h)
+        h = Dense(2048, activation='relu')(h)
+        h = Dropout(0.5)(h)
+        h = Dense(2048, activation='relu')(h)
+        h = Dropout(0.5)(h)
+        h = Dense(2048, activation='relu')(h)
+        h = Dropout(0.5)(h)
+        h = Dense(2048, activation='relu')(h)
+        h = Dropout(0.5)(h)
+        y = Dense(1, activation='sigmoid')(h)
+        model = Model(
+            inputs = x,
+            outputs = y
+        )
+        model.summary()
+        model.compile(
+            optimizer='adam',
+            loss='binary_crossentropy',
+            metrics=['accuracy']
+        )
+
+        try:
+            model.load_weights(model_weights)
+            print 'Weights loaded from ' + model_weights
+        except IOError:
+            print 'No pre-trained weights found'
+
+        try:
+            model.fit(images.reshape(images.shape[0], -1), labels,
+                      batch_size=args.batch_size,
+                      sample_weight=weights,
+                      epochs=args.nb_epochs,
+                      verbose=1,
+                      callbacks = [
+                        EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
+                        ModelCheckpoint(model_weights,
+                            monitor='val_loss', verbose=True, save_best_only=True)
+                      ],
+                      validation_data=(images_val.reshape(images_val.shape[0], -1), labels_val, weights_val)
+            )
+        except KeyboardInterrupt:
+            print 'Training finished early'
+
+        model.load_weights(model_weights)
+        yhat = model.predict(images_val.reshape(images_val.shape[0], -1), verbose=1, batch_size=args.batch_size)
+        # score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
+        # print 'Validation loss:', score[0]
+        # print 'Validation accuracy:', score[1]
+        np.save(predictions_file, yhat)
+
+    elif args.model == '3DCNN':
+        raise NotImplementedError('BDT method not yet implemented')
+
+
 
